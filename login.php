e76692c94c70310f10d1151a3a80db49f6db11a1
Rahim-Ahmed <sajimahmed3030@gmail.com>
2023-03-28 01:56:44 +0600

    first commit

---
+++ login.php
@@ -0,0 +1,183 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log In</title>
+    <link rel="stylesheet" href="assets/css/maicons.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+  
+    <link rel="stylesheet" href="assets/vendor/owl-carousel/css/owl.carousel.css">
+  
+    <link rel="stylesheet" href="assets/vendor/animate/animate.css">
+  
+    <link rel="stylesheet" href="assets/css/theme.css">
+    <script src="https://kit.fontawesome.com/c0a4fd738d.js" crossorigin="anonymous"></script>
+
+  
+	<link href='http://fonts.googleapis.com/css?family=Ropa+Sans' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="css/responsiveslides.css">
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+	<script src="js/responsiveslides.min.js"></script>
+
+    <link rel="stylesheet" href="css/style2.css">
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.bundle.js"></script>
+</head>
+<body>
+<header>
+    <div class="topbar ">
+      <div class="container ">
+        <div class="row ">
+          <div class="col-sm-8 text-sm ">
+            <div class="site-info ">
+              <a href="#"><span class="mai-call text-info"></span> +880 199 0000000</a>
+              <span class="divider">|</span>
+              <a href="#"><span class="mai-mail text-info"></span> - EW.healthcare.@example.com</a>
+            </div>
+          </div>
+          <div class="col-sm-4 text-right text-sm">
+            <div class="social-mini-button">
+              <a href="#"><span class="mai-logo-facebook-f"></span></a>
+              <a href="#"><span class="mai-logo-twitter"></span></a>
+              <a href="#"><span class="fab fa-whatsapp"></span></a>
+              <a href="#"><span class="mai-logo-instagram"></span></a>
+            </div>
+          </div>
+        </div> <!-- .row -->
+      </div> <!-- .container -->
+    </div> <!-- .topbar -->
+
+    <nav class="navbar navbar-expand-lg navbar-light shadow-sm">
+      <div class="container">
+       
+        <a class="navbar-brand" href="index.html"> 
+            <img src="icon/East West.png" alt="Medical carae" style="height: 50px;">
+            <span class="text-primary">EW</span>-Health Care</a>
+
+ 
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupport" aria-controls="navbarSupport" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupport">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link text-info"  href="index.html">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="#about_list">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#doc_list">Doctors</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#news_list">News</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#footer_list">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="btn btn-info ml-lg-3" href="#">Login / Register</a>
+            </li>
+          </ul>
+        </div> <!-- .navbar-collapse -->
+      </div> <!-- .container -->
+    </nav>
+  </header>
+
+    <div class="hstack justify-content-around gap-5 p-5  bg-light ">
+
+    <div class="border p-4 border-dark rounded bg-dark">
+					<div class="mb-3" style=" justify-content: center; display:flex; ">
+						  <img src="images/user.png" style="width: 200px; height:200px">
+					</div>
+					<div class="text list_1_of_2">
+						  <h3 class="text-white">User</h3>
+						  <p class="text-white">Register & Book Appointment</p>
+						  <div class="button "><span><a href="hms/user-login.php" class="btn btn-info">Click Here</a></span></div>
+				    </div>
+	</div>	
+    
+    <div class="border p-4 border-dark rounded bg-dark">
+					<div class="mb-3" style=" justify-content: center; display:flex; ">
+						  <img src="images/dr.png" style="width: 200px; height:200px">
+					</div>
+					<div class="text list_1_of_2">
+						  <h3 class="text-white">Dr. Login</h3>
+                          <p class="text-white">Only verified doctors can login here.</p>
+						  <div class="button"><span><a href="hms/doctor/" class="btn btn-info">Click Here</a></span></div>
+					</div>
+				</div>
+
+
+	<div class="border p-4 border-dark rounded bg-dark">
+					<div class="mb-3" style=" justify-content: center; display:flex; ">
+                    <img src="images/adm.png" style="width: 200px; height:200px">
+					</div>
+					<div class="text list_1_of_2">
+						  <h3 class="text-white" >Admin Login</h3>
+                          <p class="text-white">Register, Manage, Appointment etc.</p>
+						  <div class="button"><span><a href="hms/admin" class="btn btn-info">Click Here</a></span></div>
+				     </div>
+		</div>			
+			
+    </div>
+
+  
+  <footer class="page-footer" id="footer_list">
+    <div class="container">
+      <div class="row px-md-3">
+        <div class="col-sm-6 col-lg-3 py-3">
+          <h5>Company</h5>
+          <ul class="footer-menu">
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Career</a></li>
+            <li><a href="#">Editorial Team</a></li>
+            <li><a href="#">Protection</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-6 col-lg-3 py-3">
+          <h5>More</h5>
+          <ul class="footer-menu">
+            <li><a href="#">Terms & Condition</a></li>
+            <li><a href="#">Privacy</a></li>
+            <li><a href="#">Advertise</a></li>
+            <li><a href="#">Join as Doctors</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-6 col-lg-3 py-3">
+          <h5>Our partner</h5>
+          <ul class="footer-menu">
+            <li><a href="#">One-Fitness</a></li>
+            <li><a href="#">One-Drugs</a></li>
+            <li><a href="#">One-Live</a></li>
+          </ul>
+        </div>
+        <div class="col-sm-6 col-lg-3 py-3">
+          <h5>Contact</h5>
+          <p class="footer-link mt-2">Jahurul Islam City Gate, A/2 Jahurul Islam Ave, Dhaka 1212</p>
+          <a href="#" class="footer-link">+880 199 0000000</a>
+          <a href="#" class="footer-link">EW.healthcare@temporary.net</a>
+
+          <h5 class="mt-3">Social Media</h5>
+          <div class="footer-sosmed mt-3">
+            <a href="#" target="_blank"><span class="mai-logo-facebook-f"></span></a>
+            <a href="#" target="_blank"><span class="mai-logo-twitter"></span></a>
+            <a href="#" target="_blank"><span class="mai-logo-google-plus-g"></span></a>
+            <a href="#" target="_blank"><span class="mai-logo-instagram"></span></a>
+            <a href="#" target="_blank"><span class="mai-logo-linkedin"></span></a>
+          </div>
+        </div>
+      </div>
+
+      <hr>
+
+      <p id="copyright">Copyright &copy; 2023 <a href="https://github.com/Rahim-Ahmed" target="_blank">Rahim-Ahmed</a>. All right reserved</p>
+    </div>
+  </footer>
+</body>
+</html>
